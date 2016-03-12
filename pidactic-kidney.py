d473f4fe1279759e230ba237645ada4f1ebd3271
Dioberne <bgt96658@huskies.bloomu.edu>
2016-03-12 02:56:18 -0500

    Update pidactic-kidney.py

--- pidactic-kidney.py
+++ pidactic-kidney.py
@@ -1,66 +1,55 @@
 #!/usr/bin/python
 
-from __future__ import with_statement
-from multiprocessing import Process, Value
 import multiprocessing
 import random
 import time
+import math
 
+"""
+This version uses multiprocessing.map and 4 times faster than the older version that used multiprocessing.Process (With a global Value and a Lock).
+"""
 #Globals
-trials = 50000
+trials = 10000
 radius = 10
 threads = multiprocessing.cpu_count()
-#trails per thread 
-tpt = trials / threads
-#Will be the sum of all trials. Used to determine the actual expectation
-expectation = Value('d', 0.0)
-lock = multiprocessing.Lock()
+#trails per thread
+tpt = math.ceil( trials / threads )
 
+def walk(rad):
+    """Runs multiple trials of a random walk"""
 
-def walk():
-	"""Runs multiple trials of a random walk"""
-	for k in range(tpt):
-		x = 0
-		inside = True
-		steps = 0
-		while inside:
-			
-			steps = steps + 1
-			
-			flip = random.randint(0,1)	
-			
-			if flip == 1:
-				x = x + 1
-			else:
-				x = x - 1
-			
-			if x >= radius or x <= -radius:
-				 
-				with lock:
-					global expectation
-					expectation.value =  expectation.value + steps
-					# print "We made it ", expectation, " steps!"
-				inside = False
+    steps = 0
+    for k in range(tpt):
+        x = 0
+        inside = True
+        while inside:
+            steps = steps + 1
 
+            #Coin flip
+            if bool(random.getrandbits(1)):
+                x = x + 1
+            else:
+                x = x - 1
 
+            if not( -rad < x < rad): #x >= rad or x <= -rad:
+                inside = False
 
-def worker(num):
-    """Calls the walk. This could probably be avoided"""
-
-    walk()
-    #print 'Worker:', num
-    return
+    return steps / tpt
 
 
 if __name__ == '__main__':
     start = time.time()
+
     jobs = []
-    for i in range(threads):
-        p = multiprocessing.Process(target=worker, args=(i,))
-        jobs.append(p)
-        p.start()
     for k in range(threads):
-    	p.join()
-    print "The average number of steps is ", expectation.value / trials, " steps"
+        jobs.append(radius)
+
+    pool = multiprocessing.Pool(threads)
+
+    #Map runs walk(radius) for each "job" and stores the result
+    results = pool.map(walk, jobs)
+
     end = time.time()
-    print "This program took ", end - start, " seconds to run"
+    print ("The average number of steps is ", math.ceil(sum(results) / len(results)), " steps")
+    print ("This program took ", end - start, " seconds to run")
+    print ("There are ", threads, " threads and ", tpt, " trials per thread.")
